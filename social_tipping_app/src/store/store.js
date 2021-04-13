48dcf22e050b4ba3fd9d940d4b99ec578655cc92
楠瀬光 <kusunosehikari@kusunosehikarinoMacBook-Pro.local>
2021-04-13 22:51:33 +0900

    store.js>action内非同期処理をtry.catch文で修正

--- social_tipping_app/src/store/store.js
+++ social_tipping_app/src/store/store.js
@@ -48,99 +48,123 @@ export default new Vuex.Store({
     },
     actions: {
         async signUp({ commit }, userInfo) {
-            const authMyData = await firebase
-                .auth()
-                .createUserWithEmailAndPassword(
-                    userInfo.email,
-                    userInfo.password
-                );
+            try {
+                const authMyData = await firebase
+                    .auth()
+                    .createUserWithEmailAndPassword(
+                        userInfo.email,
+                        userInfo.password
+                    );
 
-            await authMyData.user.updateProfile({
-                displayName: userInfo.userName,
-            });
+                await authMyData.user.updateProfile({
+                    displayName: userInfo.userName,
+                });
 
-            await commit('setUserInfo', {
-                uid: authMyData.user.uid,
-                email: authMyData.user.email,
-                name: authMyData.user.displayName,
-            });
+                await commit('setUserInfo', {
+                    uid: authMyData.user.uid,
+                    email: authMyData.user.email,
+                    name: authMyData.user.displayName,
+                });
 
-            const db = firebase.firestore();
-            const ref = db.collection('users').doc(authMyData.user.uid);
-            ref.set({
-                id: authMyData.user.uid,
-                displayName: authMyData.user.displayName,
-                money: 1000,
-            })
-                .then(() => {
-                    router.push('/dashboard');
-                })
-                .catch((error) => {
-                    alert(error.message);
+                const db = await firebase.firestore();
+                const ref = await db
+                    .collection('users')
+                    .doc(authMyData.user.uid);
+                await ref.set({
+                    id: authMyData.user.uid,
+                    displayName: authMyData.user.displayName,
+                    money: 1000,
                 });
-        },
-        signIn({ dispatch }, userInfo) {
-            firebase
-                .auth()
-                .signInWithEmailAndPassword(userInfo.email, userInfo.password)
-                .then(() => {
-                    dispatch('updateUserInfo');
-                })
-                .catch((error) => {
-                    alert(error.message);
-                });
-        },
-        async updateUserInfo({ commit }) {
-            await firebase.auth().onAuthStateChanged((user) => {
-                if (user) {
-                    commit('setUserInfo', {
-                        uid: user.uid,
-                        email: user.email,
-                        name: user.displayName,
-                    });
-                }
-            });
-            router.push('/dashboard');
-        },
-        fetchMoneyStatus({ commit }) {
-            const db = firebase.firestore();
-            const { currentUser } = firebase.auth();
-            if (currentUser) {
-                const ref = db.collection('users').doc(currentUser.uid);
-                ref.onSnapshot((doc) => {
-                    let data = doc.data();
-                    commit('setMoneyStatus', data.money);
-                });
+                router.push('/dashboard');
+            } catch (error) {
+                alert(error.message);
             }
         },
-        logout({ commit }) {
-            firebase
-                .auth()
-                .signOut()
-                .then(() => {
-                    commit('setUserInfo', null);
-                    commit('setMoneyStatus', null);
-                    router.push('/login');
-                })
-                .catch((error) => {
-                    alert(error.message);
-                });
-        },
-        async fetchOtherUserData({ commit, getters }) {
-            const db = await firebase.firestore();
-            db.collection('users')
-                .where('displayName', '!=', getters.displayName)
-                .onSnapshot((querySnapshot) => {
-                    let otherUserData = [];
-                    querySnapshot.forEach((doc) => {
-                        otherUserData.push({
-                            displayName: doc.data().displayName,
-                            money: doc.data().money,
-                        });
-                        commit('setOtherUserData', otherUserData);
+
+        signIn({ dispatch }, userInfo) {
+            try {
+                firebase
+                    .auth()
+                    .signInWithEmailAndPassword(
+                        userInfo.email,
+                        userInfo.password
+                    )
+                    .then(() => {
+                        dispatch('updateUserInfo');
                     });
-                });
+            } catch (error) {
+                alert(error.message);
+            }
         },
+
+        async updateUserInfo({ commit }) {
+            try {
+                await firebase.auth().onAuthStateChanged((user) => {
+                    if (user) {
+                        commit('setUserInfo', {
+                            uid: user.uid,
+                            email: user.email,
+                            name: user.displayName,
+                        });
+                    }
+                });
+                router.push('/dashboard');
+            } catch (error) {
+                alert(error.message);
+            }
+        },
+
+        fetchMoneyStatus({ commit }) {
+            try {
+                const db = firebase.firestore();
+                const { currentUser } = firebase.auth();
+                if (currentUser) {
+                    const ref = db.collection('users').doc(currentUser.uid);
+                    ref.onSnapshot((doc) => {
+                        let data = doc.data();
+                        commit('setMoneyStatus', data.money);
+                    });
+                }
+            } catch (error) {
+                alert(error.message);
+            }
+        },
+
+        logout({ commit }) {
+            try {
+                firebase
+                    .auth()
+                    .signOut()
+                    .then(() => {
+                        commit('setUserInfo', null);
+                        commit('setMoneyStatus', null);
+                        router.push('/login');
+                    });
+            } catch (error) {
+                alert(error.message);
+            }
+        },
+
+        fetchOtherUserData({ commit, getters }) {
+            try {
+                const db = firebase.firestore();
+                db.collection('users')
+                    .where('displayName', '!=', getters.displayName)
+                    .onSnapshot((querySnapshot) => {
+                        let otherUserData = [];
+                        querySnapshot.forEach((doc) => {
+                            otherUserData.push({
+                                displayName: doc.data().displayName,
+                                money: doc.data().money,
+                            });
+                            commit('setOtherUserData', otherUserData);
+                        });
+                    });
+            } catch (error) {
+                alert(error.message);
+            }
+        },
+
         clickUserStatus({ commit }, userData) {
             commit('setClickUserStatus', userData);
             commit('setIsWalletModalShow', true);
